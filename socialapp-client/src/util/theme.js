53526f619ee37f7cd0760c428a558b08e2a0a707
biratlamichhane <lcbirat@gmail.com>
2020-05-01 00:47:32 -0500

    Profile edit function and logout button done

--- socialapp-client/src/util/theme.js
+++ socialapp-client/src/util/theme.js
@@ -1,9 +1,9 @@
 export default {
   palette: {
     primary: {
-      light: "#33c9dc",
-      main: "#00bcd4",
-      dark: "#008394",
+      light: "#369B36",
+      main: "#369B36",
+      dark: "#1C491C",
       contrastText: "#fff",
     },
     secondary: {
